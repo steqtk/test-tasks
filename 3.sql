bf059f3d1373a0e5fd6f01e803996afc8653c6bd
steQ <setfrom@gmail.com>
2018-06-19 16:38:56 +0300

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -2,6 +2,6 @@ SELECT *, COUNT(*) FROM models, cars WHERE models.id=cars.model_id GROUP BY name
 
 SELECT * FROM cars WHERE model_id NOT IN (SELECT id FROM models)
 
-SELECT models.name,sum(`price`) FROM models, cars WHERE models.id=cars.model_id AND cars.price>10  GROUP BY name HAVING SUM(`price`)>60
+SELECT models.name, SUM(`price`) FROM models, cars WHERE models.id=cars.model_id AND cars.price>10  GROUP BY name HAVING SUM(`price`)>60
 
 
